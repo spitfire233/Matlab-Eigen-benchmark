aa5e614170190a538369241f6c101418d1596e33
spitfire233 <87020972+spitfire233@users.noreply.github.com>
2026-04-01 01:58:28 +0200

    Removed initial logic pending refactor

--- main.cpp
+++ main.cpp
@@ -1,27 +1,9 @@
-#include <unsupported/Eigen/SparseExtra>
-#include <Eigen/Sparse>
 #include <filesystem>
 #include <string>
 #include <vector>
-#include <matio.h>
+#include <Eigen/Sparse>
 using namespace std;
 using namespace filesystem;
-using namespace Eigen;
-
-
-
-
-SparseMatrix<double> read_matrix_from_file(const path& matrix_file) {
-    SparseMatrix<double> A;
-    if (!matrix_file.has_extension())
-        throw runtime_error("File  " + matrix_file.filename().string() + " has no extension!");
-
-    if (matrix_file.extension().string() == ".mtx") {
-
-    }
-    return A;
-}
-
 
 int main(int argc, char* argv[]) {
     // Get the directory from command line
@@ -33,17 +15,16 @@ int main(int argc, char* argv[]) {
                           "Usage: matlab_eigen_benchmark <directory with matrix files>");
 
     // Dynamic array with files path for matrices files
-    vector<path> files;
+    vector<string> files;
 
     // Recover all matrices files from the directory and all its subdirectories
-    for (const directory_entry& entry: recursive_directory_iterator(directory,
-                                                                    directory_options::skip_permission_denied)) {
-        if (entry.is_regular_file())
-            files.emplace_back(entry.path());
+    for (const directory_entry& entry: directory_iterator(directory)) {
+        if (entry.is_regular_file() && entry.path().has_extension() && entry.path().extension() == ".mtx")
+            files.emplace_back(entry.path().string());
     }
 
-    for (const path& matrix_file : files) {
-        SparseMatrix<double> A = read_matrix_from_file(matrix_file);
+    for (const string& matrix_file : files) {
+        // Load a sparse matrix from .mtx
     }
 
     return 0;
