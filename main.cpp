830cb8e04ba8eddd49b83816f2cd73f6820b8288
spitfire233 <87020972+spitfire233@users.noreply.github.com>
2026-04-01 12:02:04 +0200

    Added fast_matrix_market

--- main.cpp
+++ main.cpp
@@ -1,30 +1,35 @@
 #include <filesystem>
 #include <string>
 #include <vector>
+#include <fstream>
+#include <fast_matrix_market/app/Eigen.hpp>
 #include <Eigen/Sparse>
-using namespace std;
-using namespace filesystem;
 
-int main(int argc, char* argv[]) {
-    // Get the directory from command line
-    const path directory = argv[1];
 
+std::vector<std::string> get_matrix_files(const std::filesystem::path& directory) {
     // Check if the directory is valid
     if (directory.empty() || !is_directory(directory))
-        throw logic_error("Must specify a valid directory! "
-                          "Usage: matlab_eigen_benchmark <directory with matrix files>");
-
-    // Dynamic array with files path for matrices files
-    vector<string> files;
-
+        throw std::logic_error("Must specify a valid directory!");
+    std::vector<std::string> files;
     // Recover all matrices files from the directory and all its subdirectories
-    for (const directory_entry& entry: directory_iterator(directory)) {
+    for (const std::filesystem::directory_entry& entry: std::filesystem::directory_iterator(directory)) {
         if (entry.is_regular_file() && entry.path().has_extension() && entry.path().extension() == ".mtx")
             files.emplace_back(entry.path().string());
     }
+    return files;
+}
 
-    for (const string& matrix_file : files) {
-        // Load a sparse matrix from .mtx
+int main(int argc, char* argv[]) {
+    // Get the directory from command line
+    if (argc < 2)
+        throw std::logic_error("Usage: matlab_eigen_benchmark <directory with matrix files>");
+    const std::filesystem::path directory = argv[1];
+
+    Eigen::SparseMatrix<double> A;
+    for (const std::vector<std::string> files = get_matrix_files(directory); const std::string& matrix_file : files) {
+        std::ifstream stream(matrix_file);
+        fast_matrix_market::read_matrix_market_eigen(stream, A);
+        std::cout << A.cols() << " " << A.rows() << std::endl;
     }
 
     return 0;
