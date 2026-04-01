517ea3bf46d0e713ab50b495165077d88a1aa9db
spitfire233 <87020972+spitfire233@users.noreply.github.com>
2026-04-01 23:02:26 +0200

    Implemented baseline for benchmark

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <fstream>
 #include <fast_matrix_market/app/Eigen.hpp>
 #include <Eigen/Sparse>
+#include <Eigen/src/SparseCholesky/SimplicialCholesky.h>
 
 
 std::vector<std::string> get_matrix_files(const std::filesystem::path& directory) {
@@ -19,7 +20,7 @@ std::vector<std::string> get_matrix_files(const std::filesystem::path& directory
     return files;
 }
 
-int main(int argc, char* argv[]) {
+int main(const int argc, char* argv[]) {
     // Get the directory from command line
     if (argc < 2)
         throw std::logic_error("Usage: matlab_eigen_benchmark <directory with matrix files>");
@@ -29,12 +30,35 @@ int main(int argc, char* argv[]) {
     for (const std::vector<std::string> files = get_matrix_files(directory); const std::string& matrix_file : files) {
         std::ifstream stream(matrix_file);
         fast_matrix_market::read_matrix_market_eigen(stream, A);
-        std::cout << A.cols() << " " << A.rows() << std::endl;
-    }
+        stream.close();
 
+        // Create xe vector of ones and create vector b as b = A * xe
+        Eigen::VectorXd xe = Eigen::VectorXd::Ones(A.rows());
+        Eigen::VectorXd b = A * xe;
+
+        // Initialize the Sparse Linear Matrix solver for applying Cholesky
+        Eigen::SimplicialLLT<Eigen::SparseMatrix<double>> solver;
+
+        // Compute the Cholesky factorization
+        solver.compute(A);
+
+        // Check if the Cholesky factorization has converged, thus if the matrix is definite positive
+        if (solver.info() != Eigen::Success) {
+            std::cerr << "Decomposition failed for: " << matrix_file << std::endl;
+            continue;
+        }
+
+        // Solve the linear system Ax = b
+        Eigen::VectorXd x = solver.solve(b);
+
+        // Check if the solver has had success solving the system with the Cholesky decomposition of A
+        if (solver.info() != Eigen::Success) {
+            std::cerr << "Solving failed for: " << matrix_file << std::endl;
+            continue;
+        }
+        // Output result or residual for verification
+        double relative_error = (x - xe).norm() / xe.norm();
+        std::cout << "Matrix: " << matrix_file << " | Relative Error: " << relative_error << std::endl;
+    }
     return 0;
 }
-
-
-
-
