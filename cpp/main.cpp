cb8784cd69335f33b7282a5bebcef76ecc9e239d
spitfire233 <87020972+spitfire233@users.noreply.github.com>
2026-04-25 18:59:48 +0200

    Implemented usage of LAPLACK and moved functions to utils.hpp

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,81 +1,23 @@
 #include <filesystem>
 #include <string>
 #include <vector>
-#include <fstream>
-#include <fast_matrix_market/app/Eigen.hpp>
-#include <Eigen/CholmodSupport>
-#include <chrono>
 #include "utils/utils.hpp"
 
 
 
 int main(const int argc, char* argv[]) {
-    // Get the directory from command line
-    if (argc < 2)
-        throw std::logic_error("Usage: matlab_eigen_benchmark <directory with matrix files>");
-    const std::filesystem::path directory = argv[1];
-
     Eigen::SparseMatrix<double> A;
+    for (const std::vector<std::string> files = get_matrix_files(MATRICES_FOLDER); const std::string& matrix_file : files) {
 
-    benchmark_results benchmark_results;
-    for (const std::vector<std::string> files = get_matrix_files(directory); const std::string& matrix_file : files) {
-        // Start the timer to measure total resolve time
-        auto start = std::chrono::high_resolution_clock::now();
+        auto [matrix_name,
+              rows,
+              cols,
+              time_elapsed, relative_error] = calculate_matrix(matrix_file);
 
-        std::ifstream stream(matrix_file);
-        fast_matrix_market::read_matrix_market_eigen(stream, A);
-        stream.close();
-
-        // Get memory allocated after reading
-        size_t mem_after_reading = get_current_setsize();
-
-        // Create xe vector of ones and create vector b as b = A * xe
-        Eigen::VectorXd xe = Eigen::VectorXd::Ones(A.rows());
-        Eigen::VectorXd b = A * xe;
-
-        // Initialize the Sparse Linear Matrix solver for applying Cholesky
-        // Eigen::SimplicialLLT<Eigen::SparseMatrix<double>> solver;
-        Eigen::CholmodSimplicialLLT<Eigen::SparseMatrix<double>> solver;
-
-        // Compute the Cholesky factorization
-        solver.compute(A);
-
-        // Check if the Cholesky factorization has converged, thus if the matrix is definite positive
-        if (solver.info() != Eigen::Success) {
-            std::cerr << "Decomposition failed for: " << matrix_file << std::endl;
-            continue;
-        }
-
-        // Solve the linear system Ax = b
-        Eigen::VectorXd x = solver.solve(b);
-
-        // Get memory allocated after solving the system
-        size_t memory_after_solving = get_current_setsize();
-
-        // Stop the timer
-        auto end = std::chrono::high_resolution_clock::now();
-
-        // Check if the solver has had success solving the system with the Cholesky decomposition of A
-        if (solver.info() != Eigen::Success) {
-            std::cerr << "Solving failed for: " << matrix_file << std::endl;
-            continue;
-        }
-
-        // Calculate results for the current matrix
-        benchmark_results.matrix_name = std::filesystem::path(matrix_file).stem().string(); // Get name of the matrix
-        benchmark_results.rows = A.rows(); // Matrix rows
-        benchmark_results.cols = A.cols(); // Matrix cols
-        benchmark_results.relative_error = (x - xe).norm() / xe.norm(); // Relative error
-        benchmark_results.time_elapsed = end - start; // Time to solve the system
-        benchmark_results.memory_used = (memory_after_solving - mem_after_reading) / (1024 * 1024); // Memory used (RSS size)
-
-        write_to_csv_file(benchmark_results);
-
-        std::cout << "Matrix: " << benchmark_results.matrix_name
-                << " | Dims:" << benchmark_results.rows << " x " << benchmark_results.cols
-                << " | Relative Error: " << benchmark_results.relative_error
-                << " | Time to solve: " << benchmark_results.time_elapsed
-                << " | Memory used: " << benchmark_results.memory_used << "MB" << std::endl;
+        std::cout << "Matrix: " << matrix_name
+                << " | Dims:" << rows << " x " << cols
+                << " | Relative Error: " << relative_error
+                << " | Time to solve: " << time_elapsed << std::endl;
     }
     return 0;
 }
--- cpp/utils/utils.hpp
+++ cpp/utils/utils.hpp
@@ -1,6 +1,11 @@
 #ifndef MATLAB_EIGEN_BENCHMARK_MEMORY_UTILS_H
 #define MATLAB_EIGEN_BENCHMARK_MEMORY_UTILS_H
+#define MATRICES_FOLDER "../matrices"
 #include <iostream>
+#include <fstream>
+#include <fast_matrix_market/app/Eigen.hpp>
+#include <Eigen/CholmodSupport>
+#include <chrono>
 #ifdef _WIN32
 #include <windows.h>
 #include <psapi.h>
@@ -11,27 +16,11 @@
 
 typedef struct BENCHMARK_RESULTS {
     std::string matrix_name;
-    Eigen::Index rows;
-    Eigen::Index cols;
-    std::chrono::duration<double> time_elapsed;
-    double relative_error;
-    size_t memory_used;
+    Eigen::Index rows{};
+    Eigen::Index cols{};
+    std::chrono::duration<double> time_elapsed{};
+    double relative_error{};
 } benchmark_results;
-
-inline size_t get_current_setsize() {
-#ifdef _WIN32 // If we are on Windows
-    // Get the current process handle
-    HANDLE hProcess = GetCurrentProcess();
-    // Get the process memory statistics
-    PROCESS_MEMORY_COUNTERS process_memory_counters;
-    // Get the process memory infos and check if the macro does not fail
-    if (hProcess != nullptr && GetProcessMemoryInfo(hProcess, &process_memory_counters, sizeof(process_memory_counters)))
-        return process_memory_counters.WorkingSetSize; // Return the current Set Size of the process
-    throw std::runtime_error("Error getting current process information!");
-#else
-    return 0;
-#endif
-}
 
 inline std::vector<std::string> get_matrix_files(const std::filesystem::path& directory) {
     // Check if the directory is valid
@@ -64,14 +53,64 @@ inline void write_to_csv_file(const benchmark_results &result) {
 
     // Write header only if file is new
     if (!file_exists) {
-        file << "Name;Dimensions;Time elapsed;Relative error;Memory used\n";
+        file << "Name;Dimensions;Time elapsed;Relative error\n";
     }
 
     // Write data
     file << result.matrix_name << ";"
          << result.rows << "x" << result.cols << ";"
          << result.time_elapsed.count() << ";"
-         << result.relative_error << ";"
-         << result.memory_used << "\n";
+         << result.relative_error << ";";
 }
+
+inline benchmark_results calculate_matrix(const std::string& matrix_file) {
+        benchmark_results benchmark_results;
+
+        Eigen::SparseMatrix<double> A;
+
+        // Start the timer to measure total resolve time
+        auto start = std::chrono::high_resolution_clock::now();
+
+        std::ifstream stream(matrix_file);
+        fast_matrix_market::read_matrix_market_eigen(stream, A);
+        stream.close();
+
+        // Create xe vector of ones and create vector b as b = A * xe
+        Eigen::VectorXd xe = Eigen::VectorXd::Ones(A.rows());
+        Eigen::VectorXd b = A * xe;
+
+        // Initialize the Sparse Linear Matrix solver for applying Cholesky
+        //Eigen::SimplicialLLT<Eigen::SparseMatrix<double>> solver;
+        Eigen::CholmodSimplicialLLT<Eigen::SparseMatrix<double>> solver;
+
+        // Compute the Cholesky factorization
+        solver.compute(A);
+
+        // Check if the Cholesky factorization has converged, thus if the matrix is definite positive
+        if (solver.info() != Eigen::Success) {
+            std::cerr << "Decomposition failed for: " << matrix_file << std::endl;
+            throw std::logic_error("Matrix is not definite positive!");
+        }
+
+        // Solve the linear system Ax = b
+        Eigen::VectorXd x = solver.solve(b);
+
+        // Stop the timer
+        auto end = std::chrono::high_resolution_clock::now();
+
+        // Check if the solver has had success solving the system with the Cholesky decomposition of A
+        if (solver.info() != Eigen::Success) {
+            std::cerr << "Solving failed for: " << matrix_file << std::endl;
+            throw std::runtime_error("The solver was not able to do the computation!");
+        }
+
+        // Calculate results for the current matrix
+        benchmark_results.matrix_name = std::filesystem::path(matrix_file).stem().string(); // Get name of the matrix
+        benchmark_results.rows = A.rows(); // Matrix rows
+        benchmark_results.cols = A.cols(); // Matrix cols
+        benchmark_results.relative_error = (x - xe).norm() / xe.norm(); // Relative error
+        benchmark_results.time_elapsed = end - start; // Time to solve the system
+        return benchmark_results;
+}
+
 #endif //MATLAB_EIGEN_BENCHMARK_MEMORY_UTILS_H
